85eb98fa87d9b55833a4c9371764ae68e408b9f4
Ramon Cunha <ramon.network@hotmail.com>
2023-07-09 15:58:33 -0300

    feat(test): add jest package

---
+++ src/entity/customer.spec.ts
@@ -0,0 +1,9 @@
+describe("Customer Unit Test", () => {
+    
+    it("should create an instance of customer", () => {
+        const result = 1;
+        expect(result).toBe(1);
+    });
+        
+    
+});
