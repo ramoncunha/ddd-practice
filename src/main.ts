6ae9c7c03016c2e826c98e2572b8942d9eb2e740
Ramon Cunha <ramon.network@hotmail.com>
2023-07-13 20:51:40 -0300

    refactor(app): created domain folder

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,18 @@
-import Customer from "./entity/customer";
-import Address from "./entity/address";
-import OrderItem from "./entity/order_item";
-import Order from "./entity/order";
-
+import Customer from "./domain/entity/customer";
+import Address from "./domain/entity/address";
+import OrderItem from "./domain/entity/order_item";
+import Order from "./domain/entity/order";
 
 // Customer Aggregate
 let customer = new Customer("1", "Ramon");
-const address = new Address("Rua das Flores", "123", "São Paulo", "SP");
+const address = new Address("Rua das Flores", 123, "São Paulo", "SP");
 customer.Address = address;
 customer.activate();
 
 // Association by ID
 
 // Order Aggregate
-const item1 = new OrderItem("1", "Item 1", 10);
-const item2 = new OrderItem("1", "Item 1", 10);
+const item1 = new OrderItem("1", "Product 1", 1, 10);
+const item2 = new OrderItem("1", "Product 2", 1, 10);
 // OrderItem e Order - association by entity
 const order = new Order("1", "1", [item1, item2]);
