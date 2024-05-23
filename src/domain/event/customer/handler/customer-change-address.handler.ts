178242c53556937fc4f7798e5da11ab85cbd9771
Ramon Cunha <ramon.network@hotmail.com>
2024-05-22 22:38:23 -0300

    feat: created customer address event

---
+++ src/domain/event/customer/handler/customer-change-address.handler.ts
@@ -0,0 +1,10 @@
+import EventHandlerInterface from "../../@shared/event-handler.interface";
+import CustomerAddress from "../customer-address.event";
+
+export default class CustomerChangeAddressHandler implements EventHandlerInterface<CustomerAddress> {
+  
+  handle(event: CustomerAddress): void {
+    console.log(`Endereço do cliente: {id}, {nome} alterado para: {endereco}`)
+  }
+
+}
