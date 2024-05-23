94b3a93f0e462fd54f8b83a1e76807bea3fb04bb
Ramon Cunha <ramon.network@hotmail.com>
2024-05-22 22:05:23 -0300

    feat: created ProductEventHandler, created SendEmailWhenProductIsCreatedHandler

--- src/domain/event/@shared/event-dispatcher.interface.ts
+++ src/domain/event/@shared/event-dispatcher.interface.ts
@@ -1,7 +1,7 @@
 import EventHandlerInterface from "./event-handler.interface";
 import EventInterface from "./event.interface";
 
-export default interface EventDispatcher {
+export default interface EventDispatcherInterface {
   notify(event: EventInterface): void;
   register(eventName: string, eventHandler: EventHandlerInterface): void;
   unregister(eventName: string, eventHandler: EventHandlerInterface): void;
