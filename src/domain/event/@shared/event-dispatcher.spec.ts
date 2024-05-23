ed4bc336139e6b73096c8045500513d82058dba7
Ramon Cunha <ramon.network@hotmail.com>
2024-05-22 22:19:17 -0300

    feat: created unregister and unregisterAll events

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -10,7 +10,32 @@ describe("Domain Events Tests", () => {
     eventDispatcher.register("ProductCreatedEvent", eventHandler);
     
     expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
-    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length ).toBe(1);
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1);
     expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+  });
+
+  it("should unregister an event handler", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+
+    eventDispatcher.unregister("ProductCreatedEvent", eventHandler);
+
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(0);
+  });
+
+  it("should unregister all events handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+
+    eventDispatcher.unregisterAll();
+
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeUndefined();
   });
 })
--- src/domain/event/@shared/event-dispatcher.ts
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -13,17 +13,25 @@ export default class EventDispatcher implements EventDispatcherInterface {
   notify(event: EventInterface): void {
     throw new Error("Method not implemented.");
   }
+
   register(eventName: string, eventHandler: EventHandlerInterface<EventInterface>): void {
    if(!this.eventHandlers[eventName]) {
     this.eventHandlers[eventName] = [];
    }
    this.eventHandlers[eventName].push(eventHandler);
   }
+
   unregister(eventName: string, eventHandler: EventHandlerInterface<EventInterface>): void {
-    throw new Error("Method not implemented.");
+    if(this.eventHandlers[eventName]) {
+      const index = this.eventHandlers[eventName].indexOf(eventHandler);
+      if (index !== -1) {
+        this.eventHandlers[eventName].splice(index, 1);
+      }
+    }
   }
+
   unregisterAll(): void {
-    throw new Error("Method not implemented.");
+    this.eventHandlers = {};
   }
 
 }
